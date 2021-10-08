7bb2ec527d0e02d5c59c6e5cd09750e1b90ef881
Amir Dora <amirdora@campus.uni-paderborn.de>
2021-10-08 23:11:26 +0200

    ngModel

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -7,12 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  serverName = "abc"
+  userName = ""
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 
 }
