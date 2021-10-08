a5cbb09d66ed8c5e3c3f3eb31fc1f702daa0bd81
Amir Dora <amirdora@campus.uni-paderborn.de>
2021-10-08 23:54:09 +0200

    ngFor

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -1,17 +1,26 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styles: [`
+  .customPara { 
+    background-color: blue;
+    color: white;
+    padding: 5px;
+   }
+  `]
 })
 export class ServerComponent implements OnInit {
 
-  userName = ""
-
+  stringList = ["secret password = tuna"]
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addParagraph(){
+    this.stringList.push("secret password = tuna " + formatDate(Date.now(),'yyyy-MM-dd HH:mm:ss','en-US'));
+  }
 }
