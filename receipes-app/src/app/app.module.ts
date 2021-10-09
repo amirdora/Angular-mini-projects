a338bee0fd498d725e7bf92884f43274da623c6d
Amir Dora <amirdora@campus.uni-paderborn.de>
2021-10-09 08:04:49 +0200

    added necessary components, categorized folders

--- receipes-app/src/app/app.module.ts
+++ receipes-app/src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './main/app.component';
+import { headerComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
+import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
+import { ReceipeItemComponent } from './receipes/receipe-item/receipe-item.component';
+import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    headerComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    ReceipeListComponent,
+    ReceipeItemComponent,
+    ReceipeDetailComponent
   ],
   imports: [
     BrowserModule
