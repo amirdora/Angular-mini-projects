84a7da9ec4bbd6b9c82a58a0ac064727eb6fbed7
Amir Dora <amirdora@campus.uni-paderborn.de>
2021-10-09 08:10:51 +0200

    minor edit

--- receipes-app/src/app/app.module.ts
+++ receipes-app/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './main/app.component';
 import { headerComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListComponent } from './shopping/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping/shopping-edit/shopping-edit.component';
 import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
-import { ReceipeItemComponent } from './receipes/receipe-item/receipe-item.component';
+import { ReceipeItemComponent } from './receipes/receipe-list/receipe-item/receipe-item.component';
 import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
 
 @NgModule({
---
+++ receipes-app/src/app/receipes/receipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receipes',
+  templateUrl: './receipes.component.html',
+  styleUrls: ['./receipes.component.css']
+})
+export class ReceipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
