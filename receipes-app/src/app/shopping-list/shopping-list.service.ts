339e0a02de493a6fd155e411eeb6ac087838be0a
Amir Dora <amirdora@campus.uni-paderborn.de>
2021-10-31 21:15:19 +0100

    shopping list cross-communication refactored

--- receipes-app/src/app/shopping-list/shopping-list.service.ts
+++ receipes-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService{
     
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ];
+      
+      getIndredients(){
+          return this.ingredients;
+      }
+
+      addIngredients(ingredient : Ingredient){
+        this.ingredients.push(ingredient);
+        console.log("ingredient added");
+      }
 }
